8e089a2de5121e5d7d7034cb8f53caa502abb387
Sn2910 <sneha.shaw29@gmail.com>
2022-07-23 16:56:48 +0200

    Query done

--- src/Components/Hackernews.js
+++ src/Components/Hackernews.js
@@ -1,3 +1,5 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import {useState, useEffect}  from 'react'
 const url = 'https://hn.algolia.com/api/v1/'
@@ -7,24 +9,32 @@ const queryParam = ''
 
 function Hackernews() {
     const [news, setNews] = useState([])
-    const [searchWord, setSearchWord,] = useState('')
-    const[query, setQuery] =useState(queryParam)
+    const [searchWord, setSearchWord] = useState('')
+    const [isLoading, setIsLoading] = useState('true')
+    const[query, setQuery] =useState("tags=front_page")
     
 
       const getData = async () => {
-        const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${searchWord}`);
+        setIsLoading(true)
+        const response = await fetch(`http://hn.algolia.com/api/v1/search?${query}`);
         console.log(response)
         if (response.ok) {
+            setIsLoading(false)
           const result = await response.json();
           console.log(result)
           setNews(result.hits)
         }
-        
+       
       }
+      useEffect(()=>{
+        getData()
+      },[query])
      
      
       /* */
-    
+      if(!isLoading)
+      return <FontAwesomeIcon icon="fa-solid fa-spinner" />
+
 
   return (
     <div className='hackernews'>
@@ -32,11 +42,11 @@ function Hackernews() {
           <h1>Hacker News</h1>
           <nav>
           <input type="" value={searchWord} onChange={(event) => setSearchWord(event.target.value)} />
-          <button onClick={getData} >Search</button>
+          <button onClick={()=>setQuery(`query=${searchWord}`)} >Search</button>
           </nav>
 
        </header>
-       <body>
+    <main>
          <ul>
             {news.map((singleNews)=>{
                 
@@ -48,7 +58,7 @@ function Hackernews() {
 
             )}
          </ul>
-       </body>
+       </main>
       
       
     </div>
